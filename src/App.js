a28321b6861b0a02efe6360a5e28306d108ecdaa
vancomm <vanushnick@gmail.com>
2022-08-04 20:03:03 +0300

    Added weather

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import Container from 'react-bootstrap/Container';
 import ProjectCard from './components/ProjectCard';
 import img from './assets/Gospers_glider_gun.gif';
+import weatherImg from './assets/weather.png';
 import './assets/app.css';
 
 export default function App() {
   return (
     <Container>
       <h1 className="my-5">Hi!</h1>
-      <ProjectCard
-        title="Conway's Game of Life"
-        text="Written in React."
-        imgSrc={img}
-        primaryLink="https://vancomm.github.io/game-of-life/"
-        secondaryLink="https://github.com/vancomm/game-of-life"
-      />
+      <div className="project-cards">
+        <ProjectCard
+          title="Conway's Game of Life"
+          text="Written in React."
+          primaryLink="https://vancomm.github.io/game-of-life/"
+          secondaryLink="https://github.com/vancomm/game-of-life"
+          imgSrc={img}
+        />
+        <ProjectCard
+          title="Weather App"
+          text="A simple React weather application"
+          primaryLink="https://vancomm.github.io/weather-app/"
+          secondaryLink="https://github.com/vancomm/weather-app"
+          imgSrc={weatherImg}
+        />
+      </div>
     </Container>
   );
 }
