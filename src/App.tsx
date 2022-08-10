a2f415a0088475d2fd6a17040811ce4b515be461
vancomm <vanushnick@gmail.com>
2022-08-10 13:00:44 +0300

    Migrated to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import ProjectCard from './components/ProjectCard';
+import img from './assets/images/Gospers_glider_gun.gif';
+import weatherImg from './assets/images/weather.png';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1 className="my-5">Hi!</h1>
+      <div className="project-cards">
+        <ProjectCard
+          title="Conway's Game of Life"
+          text="Written in React."
+          primaryLink="https://vancomm.github.io/game-of-life/"
+          secondaryLink="https://github.com/vancomm/game-of-life"
+          imgSrc={img}
+        />
+        <ProjectCard
+          title="Weather App"
+          text="A simple React weather application"
+          primaryLink="https://vancomm.github.io/weather-app/"
+          secondaryLink="https://github.com/vancomm/weather-app"
+          imgSrc={weatherImg}
+        />
+      </div>
+    </Container>
   );
 }
-
-export default App;
